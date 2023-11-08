f451b80d12d4207406d8622d3264920b7270c24b
lavender <lavenderanyango9@gmail.com>
2023-11-08 17:39:21 +0300

    rides

--- azle_rideshare/src/index.ts
+++ azle_rideshare/src/index.ts
@@ -1,9 +1,10 @@
+// src/index.ts
 import { Canister, query, text, update, Void } from 'azle';
 
 // This is a global variable that is stored on the heap
 let message = '';
 
-export default Canister({
+const CanisterMethods = Canister({
     // Query calls complete quickly because they do not go through consensus
     getMessage: query([], text, () => {
         return message;
@@ -15,3 +16,19 @@ export default Canister({
     })
 });
 
+// Custom function to display the "Welcome to Rides" message with beautiful fonts and animation
+function displayWelcomeMessage(): void {
+    if (typeof document !== 'undefined') {
+        const styledMessage = `
+            <div style="font-family: 'Arial', sans-serif; font-size: 24px; color: #ff0000; animation: fadeIn 2s ease-in-out;">
+                Welcome to Rides
+            </div>
+        `;
+        document.body.innerHTML = styledMessage;
+    }
+}
+
+// Call the custom function to display the welcome message
+displayWelcomeMessage();
+
+export default CanisterMethods;
